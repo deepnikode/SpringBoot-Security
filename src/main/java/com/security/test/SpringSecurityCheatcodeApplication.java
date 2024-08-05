dc510d1924e7e4876813d30741c96f7869921a50
deepnikode <deepnikode@gmail.com>
2024-08-05 11:02:30 +0530

    api for getting single user and all users

---
+++ src/main/java/com/security/test/SpringSecurityCheatcodeApplication.java
@@ -0,0 +1,13 @@
+package com.security.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCheatcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCheatcodeApplication.class, args);
+	}
+
+}
